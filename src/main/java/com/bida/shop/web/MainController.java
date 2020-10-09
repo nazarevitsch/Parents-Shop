ebabfaae7f7f7c4ce83ee217a20214ed330be5c7
nazarevitsch <bidaritter1997@gmail.com>
2020-10-09 12:10:59 +0300

    Added some new controllers.

--- src/main/java/com/bida/shop/web/MainController.java
+++ src/main/java/com/bida/shop/web/MainController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    @GetMapping("/")
+    public String getMainPage1(){
+        return "index";
+    }
+
     @GetMapping("/index")
-    public String getMainPage(){
+    public String getMainPage2(){
         return "index";
     }
 }
