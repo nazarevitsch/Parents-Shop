eb699d6c295c1d874f74cb77f1eee446c00ddf90
nazarevitsch <bidaritter1997@gmail.com>
2020-10-09 22:30:42 +0300

    Bug fixed with connection.t

--- src/main/java/com/bida/shop/ShopApplication.java
+++ src/main/java/com/bida/shop/ShopApplication.java
@@ -3,7 +3,7 @@ package com.bida.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.bida.shop"})
 public class ShopApplication {
 
 	public static void main(String[] args) {
